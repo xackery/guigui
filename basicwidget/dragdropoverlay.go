d37589dfcc34bf5af655128376c340ff2a4c2dea
Hajime Hoshi <hajimehoshi@gmail.com>
2024-12-30 18:47:30 +0900

    basicwidget: add DragDropOverlay

---
+++ basicwidget/dragdropoverlay.go
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2024 Hajime Hoshi
+
+package basicwidget
+
+import (
+	"image"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+
+	"github.com/hajimehoshi/guigui"
+)
+
+type DragDropOverlay struct {
+	guigui.DefaultWidgetBehavior
+
+	object any
+}
+
+type DragDropOverlayEventType int
+
+const (
+	DragDropOverlayEventTypeDropped DragDropOverlayEventType = iota
+)
+
+type DragDropOverlayEvent struct {
+	Type   DragDropOverlayEventType
+	Object any
+}
+
+func (d *DragDropOverlay) IsDragging() bool {
+	return d.object != nil
+}
+
+func (d *DragDropOverlay) Start(object any) {
+	d.object = object
+}
+
+func (d *DragDropOverlay) HandleInput(context *guigui.Context, widget *guigui.Widget) guigui.HandleInputResult {
+	if d.object != nil {
+		if inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
+			if image.Pt(ebiten.CursorPosition()).In(widget.VisibleBounds()) {
+				widget.EnqueueEvent(DragDropOverlayEvent{
+					Type:   DragDropOverlayEventTypeDropped,
+					Object: d.object,
+				})
+			}
+			d.object = nil
+			return guigui.HandleInputResult{}
+		}
+		if !ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+			d.object = nil
+		}
+		return guigui.HandleInputResult{}
+	}
+
+	return guigui.HandleInputResult{}
+}
